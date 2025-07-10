624841496c1db55793190334f71dcf7130c038cb
cookii <xenith.contact.mail@gmail.com>
2025-07-10 19:08:19 -0400

    Forgot to add license headers

--- pasteli/require.py
+++ pasteli/require.py
@@ -1,3 +1,21 @@
+"""
+PasteLi is a Python library that handles clipboard operations.
+Copyright (C) 2025 cookiiq <xenith.contact.mail@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import shutil,platform,os,warnings,subprocess
 from . import utils,constants
 
